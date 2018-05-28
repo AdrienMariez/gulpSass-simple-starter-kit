79e31953e430ce03e747a35c58a170a586f81f3a
Raphaël Apard <raphael.apard@acolad.fr>
2018-05-28 16:28:14 +0200

    Add eslint and stylelint (sass)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
 var browserSync = require('browser-sync');
 var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -11,6 +13,9 @@ gulp.task('browserSync', function() {
 
 gulp.task('sass', function(){
     return gulp.src('assets/scss/main.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.reload({
@@ -18,7 +23,13 @@ gulp.task('sass', function(){
     }))
 });
 
-gulp.task('uglify', function() {
+gulp.task('lint', function() {
+    gulp.src('assets/js/app.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+});
+
+gulp.task('js', function() {
     gulp.src('assets/js/app.js')
     .pipe(babel({
         presets:['env']
@@ -29,9 +40,9 @@ gulp.task('uglify', function() {
     }))
 });
 
-gulp.task('watch',['browserSync','sass','uglify'], function (){
+gulp.task('watch',['browserSync','sass','lint','js'], function (){
   gulp.watch('assets/scss/main.scss', ['sass']); 
-  gulp.watch('assets/js/app.js',['uglify']);
+  gulp.watch('assets/js/app.js',['lint','js']);
 });
 
-gulp.task('default', ['browserSync','uglify','sass','watch']);
+gulp.task('default', ['browserSync','lint','js','sass','watch']);
